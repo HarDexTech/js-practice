73ee84f39b8a6ccb476caec367d46699b3d00495
HarDex <adesinayomide2@gmail.com>
2025-08-13 13:46:15 +0100

    updated

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -8,7 +8,7 @@ function fizzBuzz() {
     } else if (count % 3 === 0) {
       output.push("Fizz");
     } else if (count % 5 === 0) {
-      output.push("Bizz");
+      output.push("Buzz");
     } else {
       output.push(count);
     }
