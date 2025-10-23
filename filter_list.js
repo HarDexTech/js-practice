eab69e506cdde7bea95a38108a25c6ff9a5c6bcb
Adesina Ayomide <adesinayomide2@gmail.com>
2025-10-23 09:36:18 +0100

    Refactor filter function to improve clarity and maintainability; add console log for testing

--- filter_list.js
+++ filter_list.js
@@ -9,3 +9,22 @@ function filter_list(l) {
   return result;
 }
 console.log(filter_list([1, 2, "a", "b", 3, "5", 7, 9, "3"]));
+
+
+// filter list
+const filter = (arr, value) => {
+  const result = [];
+
+  if (arr.includes(value)) {
+    for (let i = 0; i < arr.length; i++) {
+      if(arr[i] !== value){
+        result.push(arr[i]);
+      }
+    }
+    return result;
+  }
+  else{
+    return arr;
+  }
+}
+console.log(filter([1, 2, "a", "b", 3, "5", 7, 9, "3", 4], 9));
